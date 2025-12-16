001e4a91c3f0d1a835f3a085e70f232a9eaebe83
Yang <yangthang6987@gmail.com>
2025-12-16 21:03:23 +0800

    Complete 3x3 matrix addition implementation

--- practice/main.c
+++ practice/main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include "matrix_op.h"
-
+#define SIZE 3
 int main() {
+    // 使用 SIZE 標籤
     int A[SIZE][SIZE] = {{1, 2, 3}, {2, 4, 6}, {8, 2, 3}};
     int B[SIZE][SIZE] = {{1, 2, 3}, {5, 6, 7}, {8, 9, 10}};
-    int C[SIZE][SIZE]; // 存儲結果
-
+    int C[SIZE][SIZE]; 
+    int i,j;
     vAdd(A, B, C);
 
     printf("Result of Matrix Addition:\n");
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
+    for ( i = 0; i < SIZE; i++) {
+        for ( j = 0; j < SIZE; j++) {
             printf("%d ", C[i][j]);
         }
         printf("\n");
