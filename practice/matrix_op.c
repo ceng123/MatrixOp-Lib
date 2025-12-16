238821868f791e5c205bac56058419ea6b05e726
Yang <yangthang6987@gmail.com>
2025-12-16 22:09:32 +0800

    Complete 3x3 matrix addition implementation

--- practice/matrix_op.c
+++ practice/matrix_op.c
@@ -35,3 +35,11 @@ void vTransport(int A[SIZE][SIZE], int B[SIZE][SIZE], int C[SIZE][SIZE]) {
         }
     }
 }
+void vElement_wise_Multiple(int A[SIZE][SIZE], int B[SIZE][SIZE], int C[SIZE][SIZE]){
+    int i,j;
+    for (i = 0; i < SIZE; i++) {
+        for ( j = 0; j < SIZE; j++) {
+            C[i][j] = A[i][j] * B[i][j];
+        }
+    }
+}
