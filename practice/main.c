dfb7dac11e4949fe6dc2079709cb9ca15650fe5c
Yang <yangthang6987@gmail.com>
2025-12-16 20:50:27 +0800

    Complete 3x3 matrix addition implementation

--- practice/main.c
+++ practice/main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include "matrix_op.h"
 
-int main()
-{
-    int A[3][3] = {{1, 2, 3},{2, 4, 6},{8, 2, 3}};
-    int B[3][3] = {{1, 2, 3},{5, 6, 7},{8, 9, 10}};
+int main() {
+    int A[3][3] = {{1, 2, 3}, {2, 4, 6}, {8, 2, 3}};
+    int B[3][3] = {{1, 2, 3}, {5, 6, 7}, {8, 9, 10}};
+    int C[3][3]; // 修正為 3x3
 
-    int C[100][100];
-    vAdd(A,3,B,3,C,3);
-    int i = 0;
-    for (i = 0; i < 4; i++)
-    {
-        printf("%d ", C[i]);
+    vAdd(A, B, C, 3);
+
+    printf("Result of Matrix Addition:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
     }
 
     return 0;
